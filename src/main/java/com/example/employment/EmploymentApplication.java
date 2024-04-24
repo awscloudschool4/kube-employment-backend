00a7ddc31665ca8ed3e29696ea6d7bcc8c5c82a5
ChoiYeongChan <supaul97@naver.com>
2024-04-24 10:00:34 +0900

    swagger 추가

--- src/main/java/com/example/employment/EmploymentApplication.java
+++ src/main/java/com/example/employment/EmploymentApplication.java
@@ -3,10 +3,13 @@ package com.example.employment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EmploymentApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(EmploymentApplication.class, args);
     }
 
