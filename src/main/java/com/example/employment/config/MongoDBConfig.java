a0bb47862570924aeb93d9d5d0c908c39ef809c2
ChoiYeongChan <supaul97@naver.com>
2024-05-01 22:27:13 +0900

    config 수정

--- src/main/java/com/example/employment/config/MongoDBConfig.java
+++ src/main/java/com/example/employment/config/MongoDBConfig.java
@@ -2,6 +2,7 @@ package com.example.employment.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,12 +14,18 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoDBConfig extends AbstractMongoClientConfiguration {
-    
+
     @Value("${spring.data.mongodb.uri}")
     private String uri;
 
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
+
+    @Value("${spring.data.mongodb.username}") // 사용자 이름
+    private String username;
+
+    @Value("${spring.data.mongodb.password}") // 비밀번호
+    private String password;
 
     @Override
     protected String getDatabaseName() {
@@ -31,6 +38,7 @@ public class MongoDBConfig extends AbstractMongoClientConfiguration {
 
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
+                .credential(MongoCredential.createCredential(username, getDatabaseName(), password.toCharArray())) // 사용자 이름과 비밀번호 설정
                 .build();
 
         return MongoClients.create(mongoClientSettings);
